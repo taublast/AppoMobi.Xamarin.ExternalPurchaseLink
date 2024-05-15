f06a756b6352b49161002e42a4134288b8b8a743
Nick Kovalsky <nickyboom@mail.ru>
2024-05-15 12:36:46 +0300

    built framework

---
+++ src/Swift/SwiftFrameworkProxy/build/SwiftFrameworkProxy.build/Release-iphonesimulator/SwiftFrameworkProxy.build/Objects-normal/x86_64/SwiftFrameworkProxy_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/nickyboom/Projects/AppoMobi.Xamarin.ExternalPurchaseLink/src/Swift/SwiftFrameworkProxy/build/SwiftFrameworkProxy.build/Release-iphonesimulator/SwiftFrameworkProxy.build/module.modulemap \
+  /Users/nickyboom/Projects/AppoMobi.Xamarin.ExternalPurchaseLink/src/Swift/SwiftFrameworkProxy/build/SwiftFrameworkProxy.build/Release-iphonesimulator/SwiftFrameworkProxy.build/DerivedSources/SwiftFrameworkProxy_vers.c
---
+++ src/Swift/SwiftFrameworkProxy/build/XCBuildData/aed9cdb8d127a974d64c9b0d386ece55.xcbuilddata/attachments/1ea8b5b5b69057c605ef38e092830d9a
@@ -0,0 +1,5 @@
+ extern const unsigned char SwiftFrameworkProxyVersionString[];
+ extern const double SwiftFrameworkProxyVersionNumber;
+
+ const unsigned char SwiftFrameworkProxyVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:SwiftFrameworkProxy  PROJECT:SwiftFrameworkProxy-1" "\n";
+ const double SwiftFrameworkProxyVersionNumber __attribute__ ((used)) = (double)1.;
