a7b901717bb2799474aa45b1b4f983a1bac09e70
Nick Kovalsky <nickyboom@mail.ru>
2024-05-15 12:54:01 +0300

    Delete src/Swift/SwiftFrameworkProxy/build directory

--- src/Swift/SwiftFrameworkProxy/build/SwiftFrameworkProxy.build/Release-iphoneos/SwiftFrameworkProxy.build/Objects-normal/arm64/SwiftFrameworkProxy_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/nickyboom/Projects/AppoMobi.Xamarin.ExternalPurchaseLink/src/Swift/SwiftFrameworkProxy/build/SwiftFrameworkProxy.build/Release-iphoneos/SwiftFrameworkProxy.build/module.modulemap \
-  /Users/nickyboom/Projects/AppoMobi.Xamarin.ExternalPurchaseLink/src/Swift/SwiftFrameworkProxy/build/SwiftFrameworkProxy.build/Release-iphoneos/SwiftFrameworkProxy.build/DerivedSources/SwiftFrameworkProxy_vers.c
